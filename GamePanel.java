0fdfa524ce2c17017ccc193373e56e66f6fd8439
MaMeathoven <97609417+MaMeathoven@users.noreply.github.com>
2023-01-23 12:44:28 +0100

    Add files via upload

---
+++ GamePanel.java
@@ -0,0 +1,19 @@
+import java.awg.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel implements ActionListener {
+
+    GamePanel(){
+        
+    }
+
+    @Override
+    public void actionPerformed(java.awt.event.ActionEvent e) {
+        // TODO Auto-generated method stub
+        
+    }
+
+
+}
