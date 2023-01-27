c1c931f29dadaa631348ba742191d7ce2daa511a
MaMeathoven <97609417+MaMeathoven@users.noreply.github.com>
2023-01-27 10:52:57 +0100

    Add files via upload

---
+++ SnakeGame.java
@@ -0,0 +1,13 @@
+
+
+ public class SnakeGame {
+
+   public static void main(String[] args) {
+        new GameFrame();
+   }
+
+}
+
+//nice saker som jag har lagt till
+//kan inte "svänga rakt tillbaka i sig själv"
+//BÅDE! piltangenter och WASD för att styra
